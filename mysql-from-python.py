9934d2190704dee848743849c38e54dd12c06371
Samuel Denton <samdenton1995@hotmail.co.uk>
2020-12-23 17:18:08 +0000

    Using Python to create SQL functions

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -7,17 +8,19 @@ username = os.getenv('C9_USER')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Fred', 'Fred']
+        # Prepare the string with the same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE Name in ({});".format(format_strings), list_of_names)
+        connection.commit()
+
 finally:
     # Close the connection, regardless of whether or not the above was successful
     connection.close()
